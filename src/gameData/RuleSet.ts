be16b95ec8dc4cb84e52f578e1cbb2a9a3beadfa
Julian Lawrance <julian.lawrance@gmail.com>
2020-09-22 10:04:43 +0100

    Create RuleBuilder class to enable builder pattern for RuleSet class

--- src/gameData/RuleSet.ts
+++ src/gameData/RuleSet.ts
@@ -25,22 +25,78 @@ export class RuleSet {
     /**
      * Creates a new rule set.
      */
-    private constructor(
-        pairsOfPiles: number,
-        jumpBackSize: number,
-        topLimit: number,
-        handSize: number,
-    ) {
-        this.pairsOfPiles = pairsOfPiles
-        this.jumpBackSize = jumpBackSize
-        this.topLimit = topLimit
-        this.handSize = handSize
+    constructor() {
+        this.pairsOfPiles = 2
+        this.jumpBackSize = 10
+        this.topLimit = 100
+        this.handSize = 8
     }
 
     /**
      * Creates the default rule set.
      */
     static default() {
-        return new RuleSet(2, 10, 100, 8)
+        return new RuleSetBuilder()
+            .withPairsOfPiles(2)
+            .withJumpBackSize(10)
+            .withTopLimit(100)
+            .withHandSize(8)
+            .build()
+    }
+}
+
+/**
+ * Builder for a rule set.
+ */
+export class RuleSetBuilder {
+    /**
+     * The rule set to build.
+     */
+    private readonly ruleSet: RuleSet
+
+    /**
+     * Constructor.
+     */
+    constructor() {
+        this.ruleSet = new RuleSet()
+    }
+
+    /**
+     * Sets the number of pairs of piles in the rule set.
+     */
+    withPairsOfPiles(pairsOfPiles: number) {
+        this.ruleSet.pairsOfPiles = pairsOfPiles
+        return this
+    }
+
+    /**
+     * Sets the jump back size in the rule set.
+     */
+    withJumpBackSize(jumpBackSize: number) {
+        this.ruleSet.jumpBackSize = jumpBackSize
+        return this
+    }
+
+    /**
+     * Sets the top limit in the rule set.
+     */
+    withTopLimit(topLimit: number) {
+        this.ruleSet.topLimit = topLimit
+        return this
+    }
+
+    /**
+     * Sets the hand size in the rule set.
+     */
+    withHandSize(handSize: number) {
+        this.ruleSet.handSize = handSize
+        return this
+    }
+
+    /**
+     * Builds the rule set.
+     */
+    build() {
+        return this.ruleSet
     }
 }
