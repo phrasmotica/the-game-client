aa5a43b329b0f4eefb36070ea28c7b98b951c58b
Julian Lawrance <julian.lawrance@gmail.com>
2020-09-23 20:43:45 +0100

    Add footer element

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,27 @@ import { GameBoard } from "./components/GameBoard"
 import "./App.css"
 
 function App() {
+    let gameLink = "https://boardgamegeek.com/boardgame/173090/game"
+
     return (
         <div className="App">
             <header className="App-header">
                 <div className="elements">
-                <GameBoard />
+                    <GameBoard />
+
+                    <div id="footer" className="flex-center space-around">
+                        <div>
+                            <span>
+                                Version 1.0
+                            </span>
+                        </div>
+
+                        <div>
+                            <span>
+                                Adapted from <a className="App-link" href={gameLink} target="_blank" rel="noopener noreferrer">The Game</a> by Steffen Benndorf
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </header>
         </div>
