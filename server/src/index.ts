d78038373088a74cf49bed092ef065be75f6dc36
Julian Lawrance <julian.lawrance@gmail.com>
2020-09-24 13:17:37 +0100

    Create basic server for the game

---
+++ server/src/index.ts
@@ -0,0 +1,53 @@
+import express from "express"
+import http from "http"
+import socketIo, { Socket } from "socket.io"
+
+const port = process.env.PORT || 4001
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to the server!</h1>")
+})
+
+const server = http.createServer(app)
+
+const io = socketIo(server)
+
+/**
+ * Returns the number of clients in the given room.
+ */
+function getNumberOfClientsInRoom(namespace: string, roomName: string) {
+    var clients = io.nsps[namespace].adapter.rooms[roomName]?.sockets
+    if (clients === undefined) {
+        return 0
+    }
+
+    return Object.keys(clients).length
+}
+
+/**
+ * Creates a room data message.
+ */
+function createRoomDataMessage(roomName: string) {
+    return {
+        name: roomName,
+        numberOfPlayers: getNumberOfClientsInRoom("/", roomName)
+    }
+}
+
+io.on("connection", (socket: Socket) => {
+    console.log(`Player ${socket.id} joined`)
+
+    let roomName = "game room"
+    socket.join(roomName)
+
+    io.in(roomName).emit("roomData", createRoomDataMessage(roomName))
+
+    socket.on("disconnect", () => {
+        console.log(`Player ${socket.id} left`)
+        socket.to(roomName).emit("roomData", createRoomDataMessage(roomName))
+    })
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
