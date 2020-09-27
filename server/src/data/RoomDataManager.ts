75a4607bcd556abe28516f5e14ac859e8099c832
Julian Lawrance <julian.lawrance@gmail.com>
2020-09-27 12:19:22 +0100

    Remove unused methods from RoomDataManager

--- server/src/data/RoomDataManager.ts
+++ server/src/data/RoomDataManager.ts
@@ -1,9 +1,5 @@
-import { Deck } from "../models/Deck"
 import { GameData } from "../models/GameData"
-import { Hand } from "../models/Hand"
-import { Direction, Pile } from "../models/Pile"
 import { RoomData } from "../models/RoomData"
-import { RuleSet } from "../models/RuleSet"
 
 /**
  * Class for managing room data on the server.
@@ -45,40 +41,6 @@ export class RoomDataManager {
      */
     initialise(roomName: string) {
         this.roomGameData[roomName] = RoomData.default()
-    }
-
-    /**
-     * Creates a deck for the rule set.
-     */
-    createDeck(ruleSet: RuleSet) {
-        return Deck.create(2, ruleSet.topLimit)
-    }
-
-    /**
-     * Creates a hand for the rule set from the given deck.
-     */
-    createHand(ruleSet: RuleSet, deck: Deck) {
-        return new Hand(deck.draw(ruleSet.handSize))
-    }
-
-    /**
-     * Creates piles for the rule set.
-     */
-    createPiles(ruleSet: RuleSet) {
-        let piles = []
-
-        for (let i = 0; i < ruleSet.pairsOfPiles; i++) {
-            let pile = new Pile(i, 1, Direction.Ascending)
-            piles.push(pile)
-        }
-
-        for (let j = 0; j < ruleSet.pairsOfPiles; j++) {
-            let index = ruleSet.pairsOfPiles + j
-            let pile = new Pile(index, ruleSet.topLimit, Direction.Descending)
-            piles.push(pile)
-        }
-
-        return piles
     }
 
     /**
